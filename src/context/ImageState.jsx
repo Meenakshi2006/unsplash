ac87205515c9a2be419ade704647f56964a6843c
Meenakshi2006 <154886137+Meenakshi2006@users.noreply.github.com>
2024-10-24 19:34:03 +0530

    Add files via upload

---
+++ src/context/ImageState.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import ImageContext from "./ImageContext";
+import PropTypes from "prop-types";
+import { toast } from "react-toastify";
+
+function ImageState({ children }) {
+  const [images, setImages] = React.useState([]);
+  const [imageData, setImageData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  // backend url :
+  // const url = "https://unsplashbackend.onrender.com/";
+  const url = "http://localhost:3000/";
+
+  // Function to fetch images from the API
+  const fetchImages = async () => {
+    try {
+      const response = await fetch(url + "photos");
+      const data = await response.json();
+      setImageData(data.photos);
+    } catch (error) {
+      console.log("Error fetching images:", error);
+    }
+  };
+
+  // Function to add an image using the API
+  const addImage = async (label, imageUrl) => {
+    try {
+      const imageData = {
+        imageUrl,
+        label,
+      };
+
+      setLoading(true);
+
+      const token = localStorage.getItem("token");
+      // Make API request to add image
+      const response = await fetch(url + "photos/add", {
+        method: "POST",
+        body: JSON.stringify(imageData),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      toast.success(data.message);
+      fetchImages();
+    } catch (error) {
+      console.log("Error adding image:", error);
+    }
+    setLoading(false);
+  };
+
+  // Function to delete an image by id and password
+  const deleteImage = async (imageId, password) => {
+    try {
+      const token = localStorage.getItem("token");
+      setLoading(true);
+      // Make API request to delete image
+      const response = await fetch(`${url}photos/delete/${imageId}`, {
+        method: "DELETE",
+        body: JSON.stringify({ password }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      toast.success(data.message);
+      fetchImages();
+    } catch (error) {
+      console.error("Error deleting image:", error);
+    }
+    setLoading(false);
+  };
+
+  // Provide the context with the image-related functions and data
+  const imageContextValue = {
+    fetchImages,
+    images,
+    addImage,
+    deleteImage,
+    loading,
+    setLoading,
+    setImages,
+    imageData,
+  };
+
+  return (
+    <ImageContext.Provider value={imageContextValue}>
+      {children}
+    </ImageContext.Provider>
+  );
+}
+
+ImageState.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ImageState;
---
+++ src/context/User/userStates.jsx
@@ -0,0 +1,93 @@
+import UserContext from "./userContext";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+function UserStates({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  // const url = "https://unsplashbackend.onrender.com/auth/";
+  const url = "http://localhost:3000/auth/";
+
+  const navigate = useNavigate();
+  // Function to fetch images from the API
+  const profile = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      setLoading(true);
+      const response = await fetch(url + "me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      setUser(data.user);
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    }
+    setLoading(false);
+  };
+
+  // Function to fetch images from the API
+  const registerUser = async (name, email, password) => {
+    try {
+      setLoading(true);
+
+      const response = await fetch(url + "register", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json", // Ensure this header is set
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      toast.success(data.message);
+      if (data && data.success) {
+        profile();
+      }
+      navigate("/");
+    } catch (error) {
+      console.error("Error fetching images:", error);
+      toast.error(error.data.message);
+    }
+    setLoading(false);
+  };
+
+  const loginUser = async (email, password) => {
+    try {
+      setLoading(true);
+      const response = await fetch(url + "login", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json", // Ensure this header is set
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      toast.success(data.message);
+      if (data && data.success) {
+        profile();
+      }
+      navigate("/");
+    } catch (error) {
+      console.log("Error fetching images:", error);
+      toast.error(error.data.message);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ loading, registerUser, loginUser, user, profile }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+UserStates.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default UserStates;
